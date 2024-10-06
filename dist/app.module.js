a0482d03d57f4d01c0b58a1978a7a3b106128c8d
Gordon Sumit <gordon.sumit@rubicotech.in>
2024-10-06 22:45:29 +0530

    add dist
    Add sequalze
    
    add env
    
    update dist

---
+++ dist/app.module.js
@@ -0,0 +1,40 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
+const sequelize_1 = require("@nestjs/sequelize");
+const user_model_1 = require("./user.model");
+const config_1 = require("@nestjs/config");
+const process = require("process");
+const mysql2_1 = require("mysql2");
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            config_1.ConfigModule.forRoot(),
+            sequelize_1.SequelizeModule.forRoot({
+                dialect: process.env.DIALECT,
+                host: process.env.DATABASE_HOST,
+                port: parseInt(process.env.DATABSE_PORT),
+                username: process.env.DATABASE_USER,
+                password: process.env.DATABASE_PASSWORD,
+                database: process.env.DATABASE_NAME,
+                dialectModule: mysql2_1.default,
+                models: [user_model_1.User]
+            }),
+        ],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "./user.model";
+import {ConfigModule} from "@nestjs/config";
+import * as process from "process";
+import mysql2 from 'mysql2';
+import { Dialect } from 'sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: process.env.DIALECT as Dialect,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABSE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      dialectModule:mysql2,
+      models: [User]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
