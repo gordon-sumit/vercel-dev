059eb573b5924981e93d938039ee4c9b7468261d
Gordon Sumit <gordon.sumit@rubicotech.in>
2024-10-07 22:46:04 +0530

    rollback file upload code

--- src/vegetable/vegetable.controller.ts
+++ src/vegetable/vegetable.controller.ts
@@ -29,7 +29,7 @@ export class VegetableController {
                 },
             }),
         }
-    ))
+        ))
     addNewVegetable(
         @Body() formData,
         @UploadedFile() file: Express.Multer.File
