074d2ced8ced823b61e27acfa7ea4c919f61be43
Gordon Sumit <gordon.sumit@rubicotech.in>
2024-10-21 13:14:26 +0530

    add temp log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('this is test')
   app.enableCors({
     origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE"],
